90bd9bc9375f64cbd98ec466bb81df7a60759dbe
Reimirno <long_yu@berkeley.edu>
2023-11-03 15:35:59 -0700

    fix: stop exposing stack trace in health route, and make it available in prod

--- server/__init__.py
+++ server/__init__.py
@@ -75,8 +75,7 @@ import server.views  # noqa
 # registers blueprint controllers
 from server.controllers import auth_module, health_module  # noqa
 app.register_blueprint(auth_module)
-if env_value != AppEnvironment.PRODUCTION.value:
-    app.register_blueprint(health_module)
+app.register_blueprint(health_module)
 
 # registers flask cli commands
 import cli  # noqa
