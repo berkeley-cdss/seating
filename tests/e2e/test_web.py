12bf48f601d75f9ce8dac520dde1636d2b96ad5a
Reimirno <long_yu@berkeley.edu>
2023-11-03 19:23:50 -0700

    fix: spin up server for e2e tests

--- tests/e2e/test_web.py
+++ tests/e2e/test_web.py
@@ -4,3 +4,10 @@ def test_webdriver_health(driver):
     """
     driver.get('https://www.google.com')
     assert 'Google' in driver.title
+
+
+def test_local_home_page(driver):
+    """
+    Test that home page works
+    """
+    driver.get('http://localhost:5000/')
