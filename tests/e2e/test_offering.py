b1d9483634528a1e0c7667a9c1db4913a028d5cd
Reimirno <long_yu@berkeley.edu>
2023-11-22 00:18:11 -0800

    feat: add more e2e tests

--- tests/e2e/test_offering.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-def test_user_can_see_offerings(get_authed_driver, seeded_db):
-    driver = get_authed_driver("123456")
-    first_offering_btn = driver.find_element(By.CSS_SELECTOR, ".mdl-list__item-primary-content")
-    assert first_offering_btn is not None
