9610c9a9f3a69b9dd2c39d2702761d93862afddc
Reimirno <johnsonlongyu@gmail.com>
2024-02-16 13:32:05 -0800

    fix: add back the search_user scope

--- server/services/auth/scope.py
+++ server/services/auth/scope.py
@@ -2,5 +2,5 @@ scopes = [
     'url:GET|/api/v1/courses/:id',
     'url:GET|/api/v1/users/:id',
     'url:GET|/api/v1/users/:user_id/courses',
-    # 'url:GET|/api/v1/courses/:course_id/search_users'
+    'url:GET|/api/v1/courses/:course_id/search_users'
 ]
